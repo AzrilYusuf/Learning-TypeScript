893854baf10c5d93c9dbd0679862e1f9be7f179f
AzrilYusuf <azrilyusuf201003@gmail.com>
2024-07-31 14:54:26 +0700

    add(file): Add typescript file(in ./src and ./tests) Add say-hello.ts, the testing file(say-hello and datatype)

---
+++ tests/datatype.test.ts
@@ -0,0 +1,11 @@
+describe("Datatypes", function() {
+    it("should must declare", function() {
+        const name: string = "Azril";
+        const balance: number = 10000000;
+        const isVip: boolean = true;
+
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+    })
+})
--- tests/hello.test.ts
+++
@@ -1,7 +0,0 @@
-describe('Hello', function() {
-    it('should say hello', function() {
-        const name = "Hello Azril";
-
-        expect(name).toBe("Hello Azril");
-    })
-})
